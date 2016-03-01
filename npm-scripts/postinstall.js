26a95d616ded65568131f844ecc72b9b31dd3018
Michael Jackson <mjijackson@gmail.com>
2016-02-29 21:11:42 -0800

    Remove postinstall script

--- npm-scripts/postinstall.js
+++
@@ -1,7 +0,0 @@
-var stat = require('fs').stat
-var execSync = require('child_process').execSync
-
-stat('lib', function (error, stat) {
-  if (error || !stat.isDirectory())
-    execSync('npm run build', { stdio: 'inherit' })
-})
