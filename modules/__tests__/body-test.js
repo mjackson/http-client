3cb953b1067bd3224c0fb69a8d4c9978496c4a31
Michael Jackson <mjijackson@gmail.com>
2016-06-16 20:13:11 -0700

    Add tests for ASCII bodies

--- modules/__tests__/body-test.js
+++ modules/__tests__/body-test.js
@@ -1,32 +1,60 @@
 import expect from 'expect'
-import { body } from '../index'
 import byteLength from 'byte-length'
+import { body } from '../index'
 
 const echo = (input, options) =>
   Promise.resolve({ input, options })
 
-const str = 'hello λ world'
-
 describe('body', () => {
-  it('sets the body of the request', () => {
-    body(str)(echo).then(({ options }) =>
-      expect(options.body).toEqual(str)
-    )
+  describe('with an ASCII string', () => {
+    const asciiString = 'hello world'
+
+    it('sets the body of the request', () => {
+      body(asciiString)(echo).then(({ options }) =>
+        expect(options.body).toEqual(asciiString)
+      )
+    })
+
+    describe('when the content has a length property', () => {
+      it('sets the Content-Length request header', () =>
+        body(asciiString)(echo).then(({ options }) =>
+          expect(options.headers['Content-Length']).toEqual(asciiString.length)
+        )
+      )
+    })
+
+    describe('when the contentType argument is given', () => {
+      it('sets the Content-Type request header', () =>
+        body(asciiString, 'text/plain')(echo).then(({ options }) =>
+          expect(options.headers['Content-Type']).toEqual('text/plain')
+        )
+      )
+    })
   })
 
-  describe('when the content has a length property', () => {
-    it('sets the Content-Length request header', () =>
-      body(str)(echo).then(({ options }) =>
-        expect(options.headers['Content-Length']).toEqual(byteLength(str))
-      )
-    )
-  })
+  describe('with a multi-byte string', () => {
+    const multiByteString = 'hello λ world'
 
-  describe('when the contentType argument is given', () => {
-    it('sets the Content-Type request header', () =>
-      body(str, 'text/plain')(echo).then(({ options }) =>
-        expect(options.headers['Content-Type']).toEqual('text/plain')
+    it('sets the body of the request', () => {
+      body(multiByteString)(echo).then(({ options }) =>
+        expect(options.body).toEqual(multiByteString)
       )
-    )
+    })
+
+    describe('when the content has a length property', () => {
+      it('sets the Content-Length request header', () =>
+        body(multiByteString)(echo).then(({ options }) =>
+          expect(options.headers['Content-Length']).toEqual(byteLength(multiByteString))
+        )
+      )
+    })
+
+    describe('when the contentType argument is given', () => {
+      it('sets the Content-Type request header', () =>
+        body(multiByteString, 'text/plain')(echo).then(({ options }) =>
+          expect(options.headers['Content-Type']).toEqual('text/plain')
+        )
+      )
+    })
   })
 })
