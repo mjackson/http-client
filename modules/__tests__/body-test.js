d9cdefbdf4465d4c104b0960df1af5803d521222
caasi Huang <caasi.igd@gmail.com>
2016-06-17 11:08:33 +0800

    Fix Content-Length header for non-ASCII body (#8)
    * Fix Content-Length header for non-ASCII body
    
    * Use byte-length instead of `Buffer.byteLength()`

--- modules/__tests__/body-test.js
+++ modules/__tests__/body-test.js
@@ -1,27 +1,30 @@
 import expect from 'expect'
 import { body } from '../index'
+import byteLength from 'byte-length'
 
 const echo = (input, options) =>
   Promise.resolve({ input, options })
 
+const str = 'hello λ world'
+
 describe('body', () => {
   it('sets the body of the request', () => {
-    body('hello world')(echo).then(({ options }) =>
-      expect(options.body).toEqual('hello world')
+    body(str)(echo).then(({ options }) =>
+      expect(options.body).toEqual(str)
     )
   })
 
   describe('when the content has a length property', () => {
     it('sets the Content-Length request header', () =>
-      body('hello world')(echo).then(({ options }) =>
-        expect(options.headers['Content-Length']).toEqual('hello world'.length)
+      body(str)(echo).then(({ options }) =>
+        expect(options.headers['Content-Length']).toEqual(byteLength(str))
       )
     )
   })
 
   describe('when the contentType argument is given', () => {
     it('sets the Content-Type request header', () =>
-      body('hello world', 'text/plain')(echo).then(({ options }) =>
+      body(str, 'text/plain')(echo).then(({ options }) =>
         expect(options.headers['Content-Type']).toEqual('text/plain')
       )
     )
